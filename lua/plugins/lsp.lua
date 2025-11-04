1f7463866aa4b1d8d215c4a6bb927797c521ab91
OscarEl+Prron <osobando@estudiantec.cr>
2025-11-03 18:42:12 -0600

    Arreglado la kk de lsp,
    Tab para confirmar autocompletado
    Navega con Shift+Up y Shift+Down

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,7 +21,6 @@ return {
 
     end,
   },
-
   -- Autocompletado
   {
     "hrsh7th/nvim-cmp",
@@ -41,18 +40,55 @@ return {
           expand = function(args) luasnip.lsp_expand(args.body) end,
         },
         mapping = cmp.mapping.preset.insert({
+            -- Abre manualmente el menú
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+          -- ENTER: si el menú está visible, lo cierra sin aceptar
+          ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.abort()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          -- FLECHAS: si está visible, se cierran
+          ["<Up>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.abort()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<Down>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.abort()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          -- TAB: navega o confirma
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()
+              cmp.confirm({ select = true })
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             else
               fallback()
             end
           end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
+          -- navega 
+          ["<S-Down>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Up>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
