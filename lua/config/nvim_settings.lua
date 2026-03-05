b7414ab7181d5135fce079f4e550f24088b285d7
samuel <samuelzv18@gmail.com>
2026-03-05 16:20:50 -0600

    cambio de settings basicos

--- lua/config/nvim_settings.lua
+++ lua/config/nvim_settings.lua
@@ -1,6 +1,5 @@
 -- Lineas de numero
 vim.o.number = true
-vim.o.relativenumber = true
 
 -- 4 espacios para el tab
 vim.o.tabstop = 4
@@ -8,8 +7,3 @@ vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
 
---fondo
-vim.cmd [[
-	hi Normal guibg=NONE ctermbg=NONE
-	hi NormalNC guibg=NONE ctermbg=NONE
-]]
