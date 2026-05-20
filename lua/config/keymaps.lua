075a0f0de3804907d8b7c24f9eb71a43962c8d66
samuel <samuelzv18@gmail.com>
2026-05-20 16:25:56 -0600

    quité toggleterm; formatee varios archivos

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,12 +5,20 @@ vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show LSP d
 vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Buffer Delete" })
 vim.keymap.set("n", "<leader>bn", "<cmd>bn<CR>", { desc = "Buffer Next" })
 vim.keymap.set("n", "<leader>bp", "<cmd>bp<CR>", { desc = "Buffer Previous" })
+vim.keymap.set("n", "<Tab>", "<cmd>bn<CR>", { desc = "Buffer Next" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bp<CR>", { desc = "Buffer Previous" })
+vim.keymap.set("n", "<A-l>", "<cmd>bn<CR>", { desc = "Buffer Next" })
+vim.keymap.set("n", "<A-h>", "<cmd>bp<CR>", { desc = "Buffer Previous" })
 
 -- writting
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Write file" })
 
--- toggleterm
-vim.keymap.set("n", "<leader>ttf", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle Term Float" })
-
 -- quitting
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Quit Nvim" })
+vim.keymap.set("n", "<leader>q", "<cmd>qa<CR>", { desc = "Quit Nvim" })
+
+-- windows
+vim.keymap.set("n", "<C-l>", "<C-w>l<CR>", { desc = "Buffer Next" })
+vim.keymap.set("n", "<C-h>", "<C-w>h<CR>", { desc = "Buffer Previous" })
+
+--lsp
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP Go to Definition" })
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,16 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("nvim-tree").setup({
-		})
- 		vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
-		vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file tree" })
-
-	end,
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({})
+			vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
+			vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file tree" })
+		end,
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+		opts = {},
+		lazy = true,
+	},
 }
